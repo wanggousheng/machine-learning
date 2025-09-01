05e8034f379d9bda83b7fce81a3c86f646b05110
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-01 14:47:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,15 @@ with st.sidebar:
     "Your Self-Reported Health Status Score",
     ("Very Good", "Good", "Fair","Poor",'Very Poor'),
     index=None,
-    placeholder="Select contact method...",
-)
+    placeholder="Select you score.",
+  )
+  
+  adlab_c = st.selectbox(
+    "How many of the following daily living activities do you have difficulty with?
+(Note: Daily living activities include: using the toilet, feeding yourself,
+ dressing yourself, controlling bowel and bladder movements, getting in and out of bed, bathing yourself)",
+    ("0", "1", "2","3",'4','5','6'),
+    index=None,
+    placeholder="Options: 0 item (no difficulty) / 1 item 
+    / 2 items / 3 items / 4 items / 5 items / 6 items.",
+  )
