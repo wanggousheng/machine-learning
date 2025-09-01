6ad31ddb2a032f1ce8107f9a2cb5f86f0bdd0c53
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-01 14:08:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
+import joblib 
+import shap
+import matplotlib.pyplot as plt
 st.title('Prediction of Cardiovascular Disease in Middle-Aged and Elderly Patients with Diabetes Mellitus')
 
 st.info('This app is based on machine learning model')
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-
+df = pd.read_csv("X_test.csv")
+model = joblib.load('XGB.pk1')
 df
