00912da693825385c3a282fb03a9c27d680999be
wanggousheng <95527715+wanggousheng@users.noreply.github.com>
2025-09-01 10:47:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Prediction of Cardiovascular Disease in Middle-Aged and Elderly Patients with Diabetes Mellitus')
 
 st.info('This app is based on machine learning model')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+df
