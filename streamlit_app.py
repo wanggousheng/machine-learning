3b6912d8b9cf9b4c7387a808074c3908c73b025c
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-02 14:08:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,7 +179,7 @@ if st.button("Predict",width="stretch"):
   shap_values =explainer_shap.shap_values(pd.DataFrame(input_values,columns = feature_names))
   st.write(shap_values)
   st.write(explainer_shap.expected_value)
-    shap.force_plot(explainer_shap.expected_value,shap_values[:,1],pd.DataFrame(input_values,columns=feature_name),matplotlib=True)
+    shap.force_plot(explainer_shap.expected_value,shap_values,pd.DataFrame(input_values,columns=feature_name),matplotlib=True)
 
   plt.savefig(shap_force_plot.png, bbox_inches=light,dpi =1200)
   
