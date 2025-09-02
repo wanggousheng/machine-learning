38908d7c12ff46e12ba3feccdb063ac45f464c06
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-02 10:02:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,19 +150,19 @@ if st.button("Predict",width="stretch"):
   df_proba.rename(columns={0:'Disease',
                           1:'No Disease'})
   st.write(df_proba)
-# st.subheader('Predicted Species')
-# st.dataframe(df_proba,
-#             column_config={
-#             'Disease':st.column_config.ProgressColumn(
-#               'Disease',
-#               format='%f',
-#               width = 'medium',
-#               min_value =0,
-#               max_value =1),
-#               # "No Disease":st.column_config.ProgressColumn(
-#               # 'No Disease',
-#               # format='%f',
-#               # width = 'medium',
-#               # min_value =1),
-#             })
+st.subheader('Predicted Species')
+st.dataframe(df_proba,
+            column_config={
+            'Disease':st.column_config.ProgressColumn(
+              'Disease',
+              format='%f',
+              width = 'medium',
+              min_value =0,
+              max_value =1),
+              # "No Disease":st.column_config.ProgressColumn(
+              # 'No Disease',
+              # format='%f',
+              # width = 'medium',
+              # min_value =1),
+            })
 
