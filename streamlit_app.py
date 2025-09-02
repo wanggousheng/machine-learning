3c0ecf090438b6a3639a32af531d1558f9ec31f0
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-02 09:16:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.sidebar:
   hibpe = st.selectbox(
     "Has any doctor ever told you that you have hypertension?",
     (0, 1),
-    index=None,
+    index=0,
     placeholder='No = 0,Yes = 1',
   )
 
@@ -29,7 +29,7 @@ with st.sidebar:
   lunge =  st.selectbox(
     "Have you been diagnosed with Chronic lung diseases, such as chronic bronchitis ,emphysema ( excluding tumors, or cancer) by a doctor",
     (0, 1),
-    index=None,
+    index=0,
     placeholder='No = 0,Yes = 1',
   )
 
@@ -39,7 +39,7 @@ with st.sidebar:
   triglycerides (TGs),and total cholesterol, or a low high density lipoprotein level) by
   a doctor?''',
     (0, 1),
-    index=None,
+    index=0,
     placeholder='No = 0,Yes = 1',
   )
 
@@ -47,21 +47,21 @@ with st.sidebar:
   '''Have you been diagnosed with Kidney disease (except for tumor or cancer) by a
   doctor?''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
 
   digeste = st.selectbox(
   '''Have you been diagnosed with Stomach or other digestive diseases (except for tumor or cancer) by a doctor?''',
     (0, 1),
-    index=None,
+    index=0,
     placeholder='No = 0,Yes = 1',
   )
 
   asthmae = st.selectbox(
   '''Have you been diagnosed with Asthma by a doctor?''',
     (0, 1),
-    index=None,
+    index=0,
     placeholder='No = 0,Yes = 1',
   )
   
@@ -70,7 +70,7 @@ with st.sidebar:
   '''Have you been diagnosed with Memory-related disease (such as dementia, brain
   atrophy, and Parkinson’s disease) by a doctor?''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
 
@@ -80,42 +80,42 @@ with st.sidebar:
   riding a bike at a regular pace, or other activities making 
   your breathing slightly faster than usual.)''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
  
   hospital =st.selectbox(
   '''Have you received inpatient care in the past year''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
 
   retire =st.selectbox(
   '''Are you retired?''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
 
   wrist_pain = st.selectbox(
   '''Do you often have wrist pain?''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
 
   chest_pain = st.selectbox(
   '''Do you often have chest pain?''',
   (0, 1),
-  index=None,
+  index=0,
   placeholder='No = 0,Yes = 1',
   )
   
   srh = st.selectbox(
     "Your Self-Reported Health Status Score(1–5 correspond to Very Good, Good, Fair, Poor, and Very Poor (in order).)",
     (1, 2, 3, 4 ,5),
-    index=None,
+    index=0,
     placeholder="Select you score.",
   )
   srh_encoder = np.zeros(4, dtype=np.int32).reshape(1,-1)
