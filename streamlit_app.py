7b119a067cf6bbf2788c7104792af3da897eb1e4
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-25 14:32:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,99 +31,6 @@ with st.sidebar:
   #select box
   age = st.slider("How old are you?", 0, 100, 1)
 
-
-  
-  # hypertension
-  hibpe = st.selectbox(
-    "Has any doctor ever told you that you have hypertension?",
-    (0, 1),
-    index=0,
-    placeholder='No = 0,Yes = 1',
-  )
-  ## lung diseases
-  lunge =  st.selectbox(
-    "Have you been diagnosed with Chronic lung diseases, such as chronic bronchitis ,emphysema ( excluding tumors, or cancer) by a doctor",
-    (0, 1),
-    index=0,
-    placeholder='No = 0,Yes = 1',
-  )
-  ##  Dyslipidemia
-  dyslipe = st.selectbox(
-  '''Have you been diagnosed with Dyslipidemia (elevation of low density lipoprotein,
-  triglycerides (TGs),and total cholesterol, or a low high density lipoprotein level) by
-  a doctor?''',
-    (0, 1),
-    index=0,
-    placeholder='No = 0,Yes = 1',
-  )
-  # Kidney disease
-  kidneye = st.selectbox(
-  '''Have you been diagnosed with Kidney disease (except for tumor or cancer) by a
-  doctor?''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
-  # digestive diseases
-  digeste = st.selectbox(
-  '''Have you been diagnosed with Stomach or other digestive diseases (except for tumor or cancer) by a doctor?''',
-    (0, 1),
-    index=0,
-    placeholder='No = 0,Yes = 1',
-  )
-  # Asthma
-  asthmae = st.selectbox(
-  '''Have you been diagnosed with Asthma by a doctor?''',
-    (0, 1),
-    index=0,
-    placeholder='No = 0,Yes = 1',
-  )
-  #Memory-related disease
-  memrye =  st.selectbox(
-  '''Have you been diagnosed with Memory-related disease (such as dementia, brain
-  atrophy, and Parkinson’s disease) by a doctor?''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
-  ##moderate exercise
-  mdact_c = st.selectbox(
-  '''Do you usually do moderate exercise?
-  (Note: Moderate exercise includes carrying light items, 
-  riding a bike at a regular pace, or other activities making 
-  your breathing slightly faster than usual.)''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
- ##inpatient care in the past year
-  hospital =st.selectbox(
-  '''Have you received inpatient care in the past year''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
-  ##retired
-  retire =st.selectbox(
-  '''Are you retired?''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
-  ##wrist_pain
-  wrist_pain = st.selectbox(
-  '''Do you often have wrist pain?''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
-  ##chest_pain
-  chest_pain = st.selectbox(
-  '''Do you often have chest pain?''',
-  (0, 1),
-  index=0,
-  placeholder='No = 0,Yes = 1',
-  )
   ##Self-Reported Health Status Score
   srh = st.selectbox(
     "Your Self-Reported Health Status Score(1–5 correspond to Very Good, Good, Fair, Poor, and Very Poor (in order).)",
@@ -131,11 +38,7 @@ with st.sidebar:
     index=0,
     placeholder="Select you score.",
   )
-  ## encode the srh
-  srh_encoder = np.zeros(4, dtype=np.int32).reshape(1,-1)
-  if srh > 1:
-    srh_encoder[0,srh-2] = 1
-    
+
   ## daily living activities
   adlab_c = st.selectbox(
     '''How many of the following daily living activities do you have difficulty with?
@@ -146,19 +49,60 @@ with st.sidebar:
     placeholder='''Options: 0 item (no difficulty) / 1 item 
     / 2 items / 3 items / 4 items / 5 items / 6 items.''',
   )
-  # encode  the adlab
-  adlab_c_encoder = np.zeros(6, dtype=np.int32).reshape(1,-1)
-  if adlab_c > 0 :
-    adlab_c_encoder[0,adlab_c-1] = 1
+  
+  # hypertension
+  hibpe = st.selectbox(
+    "Has any doctor ever told you that you have hypertension?",
+    (0, 1),
+    index=0,
+    placeholder='No = 0,Yes = 1',
+  )
+  
+  ##  Dyslipidemia
+  dyslipe = st.selectbox(
+  '''Have you been diagnosed with Dyslipidemia (elevation of low density lipoprotein,
+  triglycerides (TGs),and total cholesterol, or a low high density lipoprotein level) by
+  a doctor?''',
+    (0, 1),
+    index=0,
+    placeholder='No = 0,Yes = 1',
+  )
+  
+  # Kidney disease
+  kidneye = st.selectbox(
+  '''Have you been diagnosed with Kidney disease (except for tumor or cancer) by a
+  doctor?''',
+  (0, 1),
+  index=0,
+  placeholder='No = 0,Yes = 1',
+  )
+  
+  ##inpatient care in the past year
+  hospital =st.selectbox(
+  '''Have you received inpatient care in the past year''',
+  (0, 1),
+  index=0,
+  placeholder='No = 0,Yes = 1',
+  )
+
+  ##chest_pain
+  chest_pain = st.selectbox(
+  '''Do you often have chest pain?''',
+  (0, 1),
+  index=0,
+  placeholder='No = 0,Yes = 1',
+  )
+
+    
+
+
 
 
 
 # merge all the input data
-values = [hibpe, lunge, dyslipe, kidneye, digeste,
-asthmae, memrye, mdact_c, hospital, retire, wrist_pain,chest_pain ]
+values = [age,srh,adlab_c,hibpe,dyslipe, kidneye, hospital,chest_pain ]
 input_values1 = np.array([values])
-input_values2 = np.concatenate([input_values1,srh_encoder], axis=1)
-input_values = np.concatenate([input_values2,adlab_c_encoder], axis=1)
+
 
 # set button for predict
 if st.button("Predict",width="stretch"):
