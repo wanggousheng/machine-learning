601a27e1d51ab3cd63ed331a1b5819c63dc191ac
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-25 15:48:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,14 +143,14 @@ if st.button("Predict",width="stretch"):
     with a predicted probability of {100* predicted_proba[0]:.1f}.%''' )
 
 
-  # # SHAP explain
-  # st.subheader("SHAP Force Plot Explanation")
-  # explainer_shap = shap.TreeExplainer(model)
-  # shap_values =explainer_shap.shap_values(input_values)
-  # shap_values
-  # # shap.force_plot(base_value=explainer_shap.expected_value,shap_values=shap_values,features=input_values,matplotlib=True)
+  # SHAP explain
+  st.subheader("SHAP Force Plot Explanation")
+  explainer_shap = shap.TreeExplainer(model)
+  shap_values =explainer_shap.shap_values(input_values)
+  shap_values
+  # shap.force_plot(base_value=explainer_shap.expected_value,shap_values=shap_values,features=input_values,matplotlib=True)
 
-  # plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
-  # st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
+  plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
+  st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
     
 
