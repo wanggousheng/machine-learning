0efe65f636e67b1b1a6c9dd7c05cb3b10500ea75
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-25 15:30:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,7 @@ if st.button("Predict",width="stretch"):
   explainer_shap = shap.TreeExplainer(model)
   shap_values =explainer_shap.shap_values(pd.DataFrame(input_values,columns = feature_names))
 
-  shap.force_plot(explainer_shap.expected_value,shap_values,input_values,matplotlib=True)
+  shap.force_plot(base_value=explainer_shap.expected_value,shap_values=shap_values,features=input_values,matplotlib=True)
 
   plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
   st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
