1404e562ba86ced11c6f7df77af60535424d44b7
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-25 14:55:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ clicking "Predict" provides personalized results, including disease risk probabi
 # get the column name for input data
 X_train = pd.read_csv("X_train.csv")
 feature_names = X_train.columns.tolist()
-st.wtrite(feature_names)
+feature_names
 stand_scaler = StandardScaler()
 X_train['Age'] = stand_scaler.fit_transform(X_train['Age'].to_frame())
 max_scaler = MinMaxScaler()
