cbb17243f2a7bafe48733bbc538d89f1b4d43a67
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-26 10:03:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,15 +148,15 @@ if st.button("Predict",width="stretch"):
     with a predicted probability of {100* predicted_proba[1]:.1f}.%''' )
 
 
-  # # SHAP explain
-  # st.subheader("SHAP Force Plot Explanation")
+  # SHAP explain
+  st.subheader("SHAP Force Plot Explanation")
   
-  # explainer_shap = shap.TreeExplainer(model)
-  # shap_values =explainer_shap.shap_values(input_values)
-  # shap_values
-  # # shap.force_plot(base_value=shap_values[0],shap_values=shap_values,features=input_values,matplotlib=True)
+  explainer_shap = shap.TreeExplainer(model)
+  shap_values =explainer_shap.shap_values(input_values)
+  shap_values
+  # shap.force_plot(base_value=shap_values[0],shap_values=shap_values,features=input_values,matplotlib=True)
 
-  # plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
-  # st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
+  plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
+  st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
     
 
