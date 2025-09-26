54dd3c6fa6dbbec78000dfacac8328558f8702c8
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-26 11:01:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,11 +153,11 @@ if st.button("Predict",width="stretch"):
   explainer_shap = shap.TreeExplainer(model)
   shap_values =explainer_shap.shap_values(input_values)
   st.write(shap_values)
-  shap_values_class = shap_values[0]
-  base_value = explainer_shap.expected_value[0] 
-  shap.force_plot(base_value,shap_values_class,features=input_values)
+  # shap_values_class = shap_values[0]
+  # base_value = explainer_shap.expected_value[0] 
+  # shap.force_plot(base_value,shap_values_class,features=input_values)
 
-  plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
-  st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
+  # plt.savefig('shap_force_plot.png', bbox_inches='tight',dpi =1600)
+  # st.image('shap_force_plot.png',caption = 'SHAP Force Plot Explanation')
     
 
