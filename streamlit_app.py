8265bcbcba00c7884006c3584f4012bddebda4ef
wang changli <95527715+wanggousheng@users.noreply.github.com>
2025-09-26 11:11:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,6 @@ if st.button("Predict",width="stretch"):
   # shap.initjs()
   explainer_shap = shap.TreeExplainer(model)
   shap_values =explainer_shap.shap_values(input_values)
-  st.write(shap_values[0,:,1])
   shap_values_class = shap_values[0,:,1]
   base_value = explainer_shap.expected_value[0] 
   shap.force_plot(base_value,shap_values_class,features=input_values, matplotlib=True)
